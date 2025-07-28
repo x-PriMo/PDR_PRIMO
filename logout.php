92d569c910bceb62db52ee17060ca27ad73a6b65
PriMo <przehabdas@wp.pl>
2025-07-28 11:37:03 +0200

    v1.2

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy(); // Zniszczenie sesji
-header("Location: index.php"); // Przekierowanie na stronę główną
-exit();
-?>
--- session_start.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start(); // Rozpoczęcie sesji tylko, jeśli nie jest aktywna
-}
-?>
